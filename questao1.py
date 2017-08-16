d4ec8f63db21e0201a118bb33715404d38d65202
Thiago Carvalho <thiago.carvalho784@gmail.com>
2017-08-15 22:21:47 -0300

    Exercicio01

---
+++ questao1.py
@@ -0,0 +1,39 @@
+
+print ("Letra A: ((2 + 3) - (5 * 8)/4) = ", end="")
+print ((2 + 3)-(5 * 8)/4)
+
+print ("Letra B: ((7 ** 2) * (4 / 2)) = ", end="")
+print ((7 ** 2) * (4 / 2))
+
+print ("Letra C: 25 % 5 = ", end="")
+print (25 % 5)
+
+print ("Letra D: (81 ** 0.5) = ", end="")
+print (81 ** 0.5)
+
+print ("Letra E: 10 > 11 and 11 < 12 = ", end="")
+print (10 > 11 and 11 < 12)
+
+print ("Letra F: (10 * 9) == (20 + 50 * 1 + 20) = ", end="")
+print (10 * 9) == (20 + 50 * 1 + 20)
+
+print ("Letra G: (7 == ( 2 * 3.5) and (False or True)) = ", end="")
+print (7 == ( 2 * 3.5) and (False or True))
+
+print ("Letra H: (7 > 2 or (7 == (2 ** 2))) = ", end="")
+print (7 > 2 or (7 == (2 ** 2)))
+
+print ("Letra I: (7 == (2 * 3.5) and (False or True)) = ", end="")
+print (7 == (2 * 3.5) and (False or True))
+
+print ("Letra J: (17 / 2) % 2 = ", end="")
+print (17 / 2) % 2
+
+print ("Letra K: not (8 < 16) or (not (18 < 6)) = ", end="")
+print (not (8 < 16) or (not (18 < 6)))
+
+print ("Letra L: (3 < 5) or (3 > 10) and (3 % 2 == 0) = ", end="")
+print (3 < 5) or (3 > 10) and (3 % 2 == 0)
+
+print ("Letra M: (14 != 3) and (3 != 2) or (14 == 0) ", end="")
+print (14 != 3) and (3 != 2) or (14 == 0)
---
+++ questao7.py
@@ -0,0 +1,4 @@
+A = int(input("Digite o primeiro número:"))
+B = int(input("Digite o segundo número:"))
+soma = A + B
+print ("A soma é igual a: %d" %soma)
